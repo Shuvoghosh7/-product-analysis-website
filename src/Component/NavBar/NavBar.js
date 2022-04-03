f85aec41c5c0071d7b367ba29edf4223f5a52a18
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-04-03 23:44:36 +0600

    add custom Link

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <div className='navBar'>
-            <Link to='/'>HOME</Link>
-            <Link to='/review'>REVIEW</Link>
-            <Link to='/dashBoard'>DASHBOARD</Link>
-            <Link to='/blogs'>BLOGS</Link>
+        <div className='text-center my-5 navBar'>
+            <CustomLink to='/'>HOME</CustomLink>
+            <CustomLink to='/review'>REVIEW</CustomLink>
+            <CustomLink to='/dashBoard'>DASHBOARD</CustomLink>
+            <CustomLink to='/blogs'>BLOGS</CustomLink>
         </div>
     );
 };
