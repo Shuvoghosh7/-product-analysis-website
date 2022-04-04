0c70c0ee3a39da8cee3f383538768d4d7877eae1
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-04-04 13:02:07 +0600

    change title name

--- src/Component/DashBoard/DashBoard.js
+++ src/Component/DashBoard/DashBoard.js
@@ -1,45 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
-    const data = [
-        {
-            "month": "Mar",
-            "investment": 100000,
-            "sell": 241,
-            "revenue": 10401
-        },
-        {
-            "month": "Apr",
-            "investment": 200000,
-            "sell": 423,
-            "revenue": 24500
-        },
-        {
-            "month": "May",
-            "investment": 500000,
-            "sell": 726,
-            "revenue": 67010
-        },
-        {
-            "month": "Jun",
-            "investment": 500000,
-            "sell": 529,
-            "revenue": 40405
-        },
-        {
-            "month": "Jul",
-            "investment": 600000,
-            "sell": 601,
-            "revenue": 50900
-        },
-        {
-            "month": "Aug",
-            "investment": 700000,
-            "sell": 670,
-            "revenue": 61000
-        }
-    ]
+    const [data,setData]=useState([])
+    console.log(data)
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res =>res.json())
+        .then(data => setData(data))
+    },[])
+
+    
     return (
         <div className='grid md:grid-cols-2 lg:grid-cols-2 gap-4 mt-10 '>
             <div className='flex justify-center'>
@@ -97,6 +68,7 @@ const DashBoard = () => {
                 <PieChart width={400} height={400}>
                     <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
                     <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                    <Tooltip />
                 </PieChart>
             </div>
         </div>
